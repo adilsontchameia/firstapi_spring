d0192ca2aacae74abf75fad812547ce501f895da
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-18 13:05:55 +0100

    feat: dependecy injection

--- src/main/java/com/adilson/firstapi/controllers/ProductController.java
+++ src/main/java/com/adilson/firstapi/controllers/ProductController.java
@@ -1,9 +1,17 @@
 package com.adilson.firstapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adilson.firstapi.repositories.ProductRepository;
+
+//Controller para utilizar todos metodos REST
 @RestController
 public class ProductController {
     // Injeção de dependência
+    @Autowired
+    ProductRepository productRepository;
+
+    // Mapeamento dos métodos
 
 }
