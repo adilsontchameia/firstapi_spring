e46b8d031a6e359343f0df58b15011c351c5674d
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-18 16:35:09 +0100

    feat: get products by id

--- src/main/java/com/adilson/firstapi/controllers/ProductController.java
+++ src/main/java/com/adilson/firstapi/controllers/ProductController.java
@@ -1,12 +1,15 @@
 package com.adilson.firstapi.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +46,17 @@ public class ProductController {
     @GetMapping("/products")
     public ResponseEntity<List<ProductsModel>> getAllProducts() {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    // GetProducts by Id
+    // Object => Porque teremos 2 tipos de respostas
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductsModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+        }
     }
 }
