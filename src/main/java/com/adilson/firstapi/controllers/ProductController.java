7c99b6001e016b95dcc7f7a5317e43bc15295730
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-18 12:57:25 +0100

    feat: controller

---
+++ src/main/java/com/adilson/firstapi/controllers/ProductController.java
@@ -0,0 +1,9 @@
+package com.adilson.firstapi.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    // Injeção de dependência
+
+}
