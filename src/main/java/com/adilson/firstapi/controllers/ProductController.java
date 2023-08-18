3919fc57fdbd3b489fce3bf55b60a3912fcf14eb
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-18 14:52:50 +0100

    feat: get all products

--- src/main/java/com/adilson/firstapi/controllers/ProductController.java
+++ src/main/java/com/adilson/firstapi/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.adilson.firstapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +37,11 @@ public class ProductController {
         // Se codigo 201 (OK) => Salva na BD
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(productRepository.save(productRepository.save(productModel)));
+    }
+
+    // GetAll Products
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductsModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
 }
