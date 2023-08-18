386776cf11e021f2bb0fe6ac2f4a7115aa2f13db
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-18 13:05:39 +0100

    feat: removed unecessary annotation

--- src/main/java/com/adilson/firstapi/repositories/ProductRepository.java
+++ src/main/java/com/adilson/firstapi/repositories/ProductRepository.java
@@ -3,13 +3,12 @@ package com.adilson.firstapi.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.adilson.firstapi.model.ProductsModel;
 
 //Repositório
 
-@Repository
+//@Repository
 public interface ProductRepository extends JpaRepository<ProductsModel, UUID> {
 
 }
